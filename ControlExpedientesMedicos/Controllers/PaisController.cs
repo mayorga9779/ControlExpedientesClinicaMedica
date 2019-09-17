88bfe48620a1b3798a27e5618b2de6d6b07e0cc9
Edgar Mayorga <ZReticuli9779@hotmail.com>
2019-09-17 17:56:42 -0600

    17/09/2019 17:55 se realizaron varias vistas, para continuar con el proyecto: - vista y controlador Clinca - vista y controlador Departamento - vista y controlador Moneda - vista y controlador Paciente - vista y controlador Pais - vista y controlador Puesto

---
+++ ControlExpedientesMedicos/Controllers/PaisController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControlExpedientesMedicos.Controllers
+{
+    public class PaisController : Controller
+    {
+        public IActionResult NuevoPais()
+        {
+            return View();
+        }
+    }
+}
