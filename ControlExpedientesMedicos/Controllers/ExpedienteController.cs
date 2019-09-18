98339be300326cb40eb9ef9241dabe1d2e76ed14
Edgar Mayorga <ZReticuli9779@hotmail.com>
2019-09-17 23:22:02 -0600

    17/09/2019 23:21 Se crearon nuevas vistas y ademas se creo la vista de la modificacion de expedientes medicos, junto las las pestnias

---
+++ ControlExpedientesMedicos/Controllers/ExpedienteController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControlExpedientesMedicos.Controllers
+{
+    public class ExpedienteController : Controller
+    {
+        public IActionResult NuevoExpediente()
+        {
+            return View();
+        }
+
+        public IActionResult ModificarExpediente()
+        {
+            return View();
+        }
+    }
+}
