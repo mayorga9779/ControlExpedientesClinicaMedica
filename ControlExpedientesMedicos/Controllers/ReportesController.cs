7ea0710fe1c82db63e4065e8d53a31a7d1842e40
Edgar Mayorga <ZReticuli9779@hotmail.com>
2019-09-18 22:53:37 -0600

    18/09/2019 22:52 Se terminaron de crear las vistas de los procesos y la vista de la modificacion de expediente medico.

---
+++ ControlExpedientesMedicos/Controllers/ReportesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControlExpedientesMedicos.Controllers
+{
+    public class ReportesController : Controller
+    {
+        public IActionResult ReportePacientes()
+        {
+            return View();
+        }
+
+        public IActionResult ReporteMedicos()
+        {
+            return View();
+        }
+
+        public IActionResult ReporteCitas()
+        {
+            return View();
+        }
+    }
+}
