8cb54eda589513f774bc396f3bcbf36c0bce6c66
Edgar Mayorga <ZReticuli9779@hotmail.com>
2019-10-10 22:13:09 -0600

    10/10/2019 22:12 Se creo el modulo de creacion de paciente, completo.

--- ControlExpedientesMedicos/Controllers/PacienteController.cs
+++ ControlExpedientesMedicos/Controllers/PacienteController.cs
@@ -2,14 +2,83 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ControlExpedientesMedicos.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ControlExpedientesMedicos.Controllers
 {
     public class PacienteController : Controller
     {
-        public IActionResult NuevoPaciente()
+        private ModeloPaciente modeloPaciente = new ModeloPaciente();
+        private String mensaje = "";
+
+        public IActionResult NuevoPaciente(IFormCollection formCollection)
         {
+            mensaje = "";
+            ViewData["mensaje"] = mensaje;
+
+            try
+            {
+                int opcion = 1;
+                String nombres = formCollection["txtNombresP"].ToString();
+                String apellidos = formCollection["txtApellidosP"].ToString();
+                String direccion = formCollection["txtDireccionP"].ToString();
+                String telefono = formCollection["txtTelefonoP"].ToString();
+                DateTime fecha;
+                String fecha_nacimiento = "";
+                String seguro_social = "";
+                String genero = "";
+                String email = "";
+
+                if (!formCollection["txtFecNacP"].ToString().Equals(""))
+                {
+                    fecha = Convert.ToDateTime(formCollection["txtFecNacP"].ToString());
+                    fecha_nacimiento = fecha.ToString("yyyyMMdd");
+
+                }
+
+                if (formCollection["txtSeguroSocial"].ToString().Equals(""))
+                {
+                    seguro_social = null;
+                }
+                else
+                {
+                    seguro_social = formCollection["txtSeguroSocial"].ToString();
+                }
+
+                if (formCollection["cbGenero"].ToString().Equals(""))
+                {
+                    genero = null;
+                }
+                else
+                {
+                    genero = formCollection["cbGenero"].ToString();
+                }
+
+                if (formCollection["txtEmailP"].ToString().Equals(""))
+                {
+                    email = null;
+                }
+                else
+                {
+                    email = formCollection["txtEmailP"].ToString();
+                }
+
+                if (!nombres.Equals("") && !apellidos.Equals("") && !direccion.Equals("") && !telefono.Equals("") && !fecha_nacimiento.Equals("") && !genero.Equals("") && !email.Equals(""))
+                {
+                    mensaje = modeloPaciente.Crear_Paciente(opcion, nombres, apellidos, direccion, telefono, fecha_nacimiento, seguro_social, genero, email);
+                    ViewData["mensaje"] = mensaje;
+                    return View(ViewData["mensaje"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                mensaje = "EXCEPTION";
+                ViewData["mensaje"] = mensaje;
+                return View(ViewData["mensaje"]);
+            }
+
             return View();
         }
     }
---
+++ ControlExpedientesMedicos/Models/ModeloPaciente.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlExpedientesMedicos.Models
+{
+    public class ModeloPaciente
+    {
+        private SqlConnection conn;
+        private Cadena_Conexion cadena = new Cadena_Conexion();
+        private String cadena_conexion = "";
+        private String mensaje = "";
+
+        public ModeloPaciente()
+        {
+            cadena_conexion = cadena.Obtener_Cadena();
+        }
+
+        public String Crear_Paciente(int opcion, String nombres, String apellidos, String direccion, String telefono, String fecha_nacimiento, String seguro_social, String genero, String email)
+        {
+            try
+            {
+                conn = new SqlConnection(cadena_conexion);
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("dbo.sp_agregar_paciente", conn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("opcion", 1);
+                cmd.Parameters.AddWithValue("nombres", nombres);
+                cmd.Parameters.AddWithValue("apellidos", apellidos);
+                cmd.Parameters.AddWithValue("direccion", direccion);
+                cmd.Parameters.AddWithValue("telefono", telefono);
+                cmd.Parameters.AddWithValue("fecha_nacimiento", fecha_nacimiento);
+                cmd.Parameters.AddWithValue("seguro_social", seguro_social);
+                cmd.Parameters.AddWithValue("genero", genero);
+                cmd.Parameters.AddWithValue("email", email);
+
+                SqlParameter pMensaje = new SqlParameter("mensaje", SqlDbType.VarChar, 8);
+                pMensaje.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(pMensaje);
+
+                cmd.ExecuteNonQuery();
+
+                mensaje = (String)pMensaje.Value;
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                mensaje = "EXCEPTION";
+            }
+
+            return mensaje;
+        }
+    }
+}
